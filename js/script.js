1bd8e5fe0ede81293cff9df5f115e57b29d2a245
sabrine tounsi <sabrine.tounsi.st@gmail.com>
2021-12-09 23:37:01 +0100

    none

--- js/script.js
+++ js/script.js
@@ -20,4 +20,65 @@ if(username){
 
  });
 
+ let products =[
+
+    { id: 1,
+      title: "Micro-casque"  ,
+      size:"Large",
+      imageUrl:"images/microcasque.jpg.",
+    },
+    { id: 2,
+        title: "Laptop"  ,
+        size:"Large",
+        imageUrl:"images/laptop.jpg",
+      },
+      { id: 3,
+        title: "Mouse"  ,
+        size:"Small",
+        imageUrl:"images/mouse.jpg",
+      },
+      { id: 4,
+        title: "Tablette"  ,
+        size:"Meduim",
+        imageUrl:"images/tablette.jpg",
+      },
+
+
+    ];
+    
+    let productsDom= document.querySelector(".products");
+
+    function drawProductUI(){
+        let productsUI = products.map((item)=>{
+            return `
+            <div class="product-item">
+            <img src="${item.imageUrl}" alt="images">
+            <div class="product-item-desc">
+                <h2>${item.title}</h2>
+                <p>lorem ipsum dolor sit amet consectetur adipiscing elit</p>
+                <span>Size: ${item.size}</span>
+            </div>
+            <div class="product-item-actions">
+                <button class="add-to-cart">Add to cart</button>
+                <i class="favorite fa fa-heart-o"></i>
+            </div>
+        </div>
+            
+            `
+        })
+
+        productsDom.innerHTML=productsUI;
+    }
+    drawProductUI();
+
+    function CheckLogedUser(){
+        if(localStorage.getItem("username")){
+            console.log("added to Cart");
+        } else{
+            window.location ="login.html";
+        }
+    }
+
+
+
 
