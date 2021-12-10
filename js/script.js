ff6d3e88f5e643557469cb7c0c77890986e324dc
c-Sabrine.Tounsi <c-sabrine.tounsi@wolterskluwer.com>
2021-12-10 16:12:01 +0100

    Modification

--- js/script.js
+++ js/script.js
@@ -45,8 +45,15 @@ if(username){
 
 
     ];
-    
+    //define products
     let productsDom= document.querySelector(".products");
+    let cartProductDom= document.querySelector(".carts-products div");
+    let cart= document.querySelector(".carts-products");
+
+    let badgeDom = document.querySelector(".badge");
+    let shoppingCart = document.querySelector(".shopping-cart");
+    shoppingCart.addEventListener('click',OpenCartMenu );
+
 
     function drawProductUI(){
         let productsUI = products.map((item)=>{
@@ -73,7 +80,11 @@ if(username){
 
     function AddedCart(id){
         let choosenItem = products.find((item) => item.id ===id)
-        console.log(choosenItem);
+        cartProductDom.innerHTML +=`<p>${choosenItem.title}</p>`;
+        let cartProductLength = document.querySelectorAll(".carts-products div p");
+        console.log(cartProductLength);
+        badgeDom.style.display = "block";
+        badgeDom.innerHTML = cartProductLength.length;
 
     }
 
@@ -86,6 +97,17 @@ if(username){
         }
     }
 
+    function OpenCartMenu(){
+   
+        if(cartProductDom.innerHTML !== ""){
+            if(cart.style.display =="block"){
+            cart.style.display ="none";
 
+            }else{
+                cart.style.display ="block";
+            }
+        } 
+
+    }
 
 
